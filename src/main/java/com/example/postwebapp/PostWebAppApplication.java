cb330a6f9836c28c431e24ad07462dc131d1464c
amr mahmoud <amrmahmoud1900@gmail.com>
2023-06-27 01:03:45 +0300

    add project Structure

---
+++ src/main/java/com/example/postwebapp/PostWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.postwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/postwebapp/PostWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
